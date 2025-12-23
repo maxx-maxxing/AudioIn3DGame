322872f145ccc80996b267cc2a384f93cd559e36
maxx-maxxing <maxxlazos93@gmail.com>
2025-12-23 09:19:37 -0700

    Finished x and y camera rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,9 +24,18 @@ public class PlayerController : MonoBehaviour
     
     void Look()
     {
-        float x = Input.GetAxis("Mouse X") * lookSensitivity;
-        float y = Input.GetAxis("Mouse Y") * lookSensitivity;
+        float x = Input.GetAxis("Mouse X") * lookSensitivity; // Amount cam moved right/left, times lookSens
+        float y = Input.GetAxis("Mouse Y") * lookSensitivity; // Amount cam moved up/down, times lookSens
 
-        transform.eulerAngles += Vector3.up * x; // rotates player around y axis
+        transform.eulerAngles += Vector3.up * x; // Rotate the Player object around the Y-axis by x degrees this frame
+        currXRotation += y; // Keeps track of net vertical movement around x-axis
+        currXRotation = Mathf.Clamp(currXRotation, minXRotation, maxXRotation); 
+        // ^^ Sets a limit on looking up/down so you cant "break neck"
+        
+        
+        cam.localEulerAngles = new Vector3(-currXRotation, 0.0f, 0.0f);
+        // ^^ Rotate around cameras x-axis, not the worlds.
+        // left/right => rotate around parent's y (transform.eulerAngles += Vector3.up * x)
+        // up/down => rotate around camera's (child) x with a new vector
     }
 }
